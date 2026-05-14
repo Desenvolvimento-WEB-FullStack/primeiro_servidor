5f7a93e9cf1b718be87590df1fed6e4404961a1b
Henrique Douglas <henriquedouglas2803@gmail.com>
2026-05-13 21:59:31 -0300

    adiciona exemplo de rota post

--- src/main.js
+++ src/main.js
@@ -1,50 +1,28 @@
 import express from "express";
 import cors from "cors";
 
-// criar váriavel pra receber o express
 const app = express();
 const PORTA_APP = 8888;
 
-const mensagens = [
-  "Acredite no seu potencial — grandes conquistas começam com um único passo.",
-  "O sucesso é a soma de pequenos esforços repetidos dia após dia.",
-  "Não espere pela oportunidade perfeita. Crie-a.",
-  "Cada dia é uma nova chance de ser melhor do que ontem.",
-  "A persistência transforma o impossível em possível.",
-  "Você é mais forte do que pensa e mais capaz do que imagina.",
-  "O único limite que existe é aquele que você mesmo cria.",
-  "Grandes resultados exigem grandes comprometimentos.",
-  "Errar faz parte do processo — desistir é a única derrota real.",
-  "Foque no progresso, não na perfeição.",
-];
-
 app.use(cors());
+app.use(express.json());
 
-app.get("/bem_vindo", (request, response) => {
-  response.send({ mensagem: "Bem vindo" });
+let contadorProdutos = 1;
+const produtos = [];
+
+app.get("/produtos", (request, response) => {
+  response.send({ produtos });
 });
 
-app.get("/mensagem", (request, response) => {
-  const numeroAleatorio = Math.random() * 10;
-  const numeroArrendodado = Math.trunc(numeroAleatorio);
+app.post("/produtos", (request, response) => {
+  const meusDados = request.body;
 
-  console.log(numeroAleatorio);
-  console.log(numeroArrendodado);
+  meusDados.id = contadorProdutos;
+  contadorProdutos++;
 
-  response.send({ mensagem: mensagens[numeroArrendodado] });
-});
+  produtos.push(meusDados);
 
-app.get("/sorteio", (request, response) => {
-  const nomesRecebidos = request.query.nomes.split(",");
-
-  const tamanhoDoArray = nomesRecebidos.length;
-  const posicaoNoArray = Math.random() * tamanhoDoArray;
-  const posicaoNoArrayArrendondada = Math.trunc(posicaoNoArray);
-
-  response.send({ resultado: nomesRecebidos[posicaoNoArrayArrendondada] });
+  response.send({ mensagem: "cadastro com sucesso" });
 });
 
 app.listen(PORTA_APP, () => console.log(" 🚀🚀🚀 Servidor rodando"));
-
-// GET POST DELETE PUT
-// path
